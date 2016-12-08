ff873b805bbb7f218e24388fd54b081037ac8093
abelmaniquis <abelmaniquis@gmail.com>
2016-12-08 22:21:41 +0000

    changed max width for responsiveness

--- js/app.js
+++ js/app.js
@@ -11,4 +11,5 @@ function fadeInElements(){
     $("#wrapper").hide(0).delay(100).fadeIn(3000);
     $("#introduction").hide(0).delay(200).fadeIn(3000);
     $(".project").hide(0).delay(300).fadeIn(3000);
+    $("footer").hide(0).delay(400).fadeIn(4000);
 }
