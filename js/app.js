40e9e65dc78d0656f8859b48f19b9846fc4edef0
abelmaniquis <abelmaniquis@gmail.com>
2016-12-08 20:54:59 +0000

    fixed some alignment issues

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 $(document).ready(portFunction);
 
 function portFunction(){
-    console.log("Hello");
+    console.log("Hello, welcome to my profile, please feel free to visit any of my above projects!");
+    fadeInElements();
+}
+
+function fadeInElements(){
+    console.log("This will fade in elements");
+    
+    $("#wrapper").hide(0).delay(100).fadeIn(3000);
+    $("#introduction").hide(0).delay(200).fadeIn(3000);
+    $(".project").hide(0).delay(300).fadeIn(3000);
 }
